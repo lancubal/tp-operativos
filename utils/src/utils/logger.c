b7c783caca6ef98f33871636a62758c72c2f3e82
Tom <tdemonasterio@frba.utn.edu.ar>
2024-04-05 17:25:07 +0000

    Se agrego la library socket con server, client y nuevas funciones a utils

---
+++ utils/src/utils/logger.c
@@ -0,0 +1,31 @@
+//
+// Created by utnso on 4/5/24.
+//
+
+#include "logger.h"
+
+void readFromConsole(t_log* logger)
+{
+    char* leido;
+
+    // La primera te la dejo de yapa
+
+    // El resto, las vamos leyendo y logueando hasta recibir un string vacío
+    while(true) {
+        leido = readline("> ");
+        if (string_is_empty(leido)) {
+            break;
+        }
+
+        log_info(logger, "%s", leido);
+    }
+    // ¡No te olvides de liberar las lineas antes de regresar!
+    free(leido);
+}
+
+t_log* loggerCreate(void)
+{
+    t_log* nuevo_logger = log_create("client.log", "client", 1, LOG_LEVEL_INFO);
+
+    return nuevo_logger;
+}
---
+++ utils/src/utils/module.h
@@ -0,0 +1,13 @@
+//
+// Created by utnso on 4/5/24.
+//
+
+#ifndef TP_2024_1C_GRUPO_MODULE_H
+#define TP_2024_1C_GRUPO_MODULE_H
+
+#include <commons/log.h>
+#include <commons/config.h>
+
+void moduleTerminate(int, t_log*, t_config*);
+
+#endif //TP_2024_1C_GRUPO_MODULE_H
