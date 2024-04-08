449015a58c9df9f126b8b2f9581b4d57a1cbefac
Tom <tdemonasterio@frba.utn.edu.ar>
2024-04-08 18:34:08 +0000

    modularice el tad de argumentos para la conexion

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -20,31 +20,32 @@ int main(int argc, char* argv[]) {
     //Obtener datos de configuracion
     cpu_config_t* cpuConfig = cpuConfigLoad(argv[1]);
 
-    // Se crea el tad para el hilo de Dispatch
+    //Se crea el tad para el hilo de Dispatch
     pthread_t dispatchServer;
-    // Se asignan los argumentos para el hilo de Dispatch
-    server_args_t* ip_puerto_dispatch = malloc(sizeof(server_args_t));
-    ip_puerto_dispatch->ip = cpuConfig->ipCPU;
-    ip_puerto_dispatch->puerto = cpuConfig->puertoEscuchaDispatch;
-    ip_puerto_dispatch->proceso = "Dispatch";
-    // Se crea el hilo de Dispatch
-    pthread_create(&dispatchServer, NULL, (void*) iniciarServerProceso, (void*) ip_puerto_dispatch);
+    //Se asignan los argumentos para el hilo de Dispatch
+    conexionArgsT* dispatchServerArgs = createConexionArgs(cpuConfig->ipCPU, cpuConfig->puertoEscuchaDispatch, "Dispatch");
+    //Se crea el hilo de Dispatch
+    pthread_create(&dispatchServer, NULL, (void*) iniciarServerProceso, (void*) dispatchServerArgs);
 
-    // Se crea el tad para el hilo de Dispatch
+    //Se crea el tad para el hilo de Interrupt
     pthread_t interruptServer;
-    // Se asignan los argumentos para el hilo de Interrupt
-    server_args_t* ip_puerto_interrupt = malloc(sizeof(server_args_t));
-    ip_puerto_interrupt->ip = cpuConfig->ipCPU;
-    ip_puerto_interrupt->puerto = cpuConfig->puertoEscuchaInterrupt;
-    ip_puerto_interrupt->proceso = "Interrupt";
-    // Se crea el hilo de Interrupt
-    pthread_create(&interruptServer, NULL, (void*) iniciarServerProceso, (void*) ip_puerto_interrupt);
-    // Se espera a que los hilos terminen
+    //Se asignan los argumentos para el hilo de Interrupt
+    conexionArgsT* interruptServerArgs = createConexionArgs(cpuConfig->ipCPU, cpuConfig->puertoEscuchaInterrupt, "Interrupt");
+    //Se crea el hilo de Interrupt
+    pthread_create(&interruptServer, NULL, (void*) iniciarServerProceso, (void*) interruptServerArgs);
+
+    //Se crea el tad para el hilo de Memoria
+    pthread_t memoriaServer;
+    //Se asignan los argumentos para el hilo de Memoria
+    conexionArgsT* memoriaServerArgs = createConexionArgs(cpuConfig->ipMemoria, cpuConfig->puertoMemoria, "Memoria");
+
+    //Se crea el hilo de Memoria
+    pthread_create(&memoriaServer, NULL, (void*) conectarA, (void*) memoriaServerArgs);
+
+    //Se inician los hilos de las conexiones
     pthread_join(interruptServer, NULL);
     pthread_join(dispatchServer, NULL);
-
-    //Conectar a memoria
-    conectarA(cpuConfig->ipMemoria, cpuConfig->puertoMemoria, "Memoria");
+    pthread_join(memoriaServer, NULL);
 
     //Finalizar
     log_destroy(logger);
