2ab6daac0290522baab630dada4c30d0c153c28e
Tom <tdemonasterio@frba.utn.edu.ar>
2024-04-14 19:24:22 +0000

    registros del CPU

---
+++ cpu/src/cpu_registros.c
@@ -0,0 +1,5 @@
+//
+// Created by utnso on 4/14/24.
+//
+
+#include "cpu_registros.h"
---
+++ cpu/src/cpu_registros.h
@@ -0,0 +1,22 @@
+//
+// Created by utnso on 4/14/24.
+//
+
+#ifndef TP_2024_1C_GRUPO_CPU_REGISTROS_H
+#define TP_2024_1C_GRUPO_CPU_REGISTROS_H
+
+#include <stdint.h>
+
+uint32_t PC;
+uint8_t AX;
+uint8_t BX;
+uint8_t CX;
+uint8_t DX;
+uint32_t EAX;
+uint32_t EBX;
+uint32_t EDX;
+uint32_t ECX;
+uint32_t SI;
+uint32_t DI;
+
+#endif //TP_2024_1C_GRUPO_CPU_REGISTROS_H
