de2ecd9189c2fb6328754fdb4aeb0ce2e15c4a5c
Tom <tdemonasterio@frba.utn.edu.ar>
2024-04-15 20:57:31 +0000

    networking creado

---
+++ utils/src/sockets/networking.c
@@ -0,0 +1,133 @@
+//
+// Created by utnso on 4/15/24.
+//
+
+#include "networking.h"
+
+void sendMessage(char* mensaje, int socketfd) {
+    t_paquete* paquete = malloc(sizeof(t_paquete));
+
+    paquete->codigo_operacion = MENSAJE;
+    paquete->buffer = malloc(sizeof(t_buffer));
+    paquete->buffer->size = strlen(mensaje) + 1;
+    paquete->buffer->stream = malloc(paquete->buffer->size);
+    memcpy(paquete->buffer->stream, mensaje, paquete->buffer->size);
+
+    int bytes = paquete->buffer->size + 2*sizeof(int);
+
+    void* a_enviar = serializar_paquete(paquete, bytes);
+
+    send(socketfd, a_enviar, bytes, 0);
+    free(a_enviar);
+    freePacket(paquete);
+}
+
+void* serializar_paquete(t_paquete* paquete, int bytes) {
+    void *magic = malloc(bytes);
+    int desplazamiento = 0;
+
+    memcpy(magic + desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+    desplazamiento += sizeof(int);
+    memcpy(magic + desplazamiento, &(paquete->buffer->size), sizeof(int));
+    desplazamiento += sizeof(int);
+    memcpy(magic + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
+    desplazamiento += paquete->buffer->size;
+
+    return magic;
+}
+
+
+void crear_buffer(t_paquete* paquete)
+{
+    paquete->buffer = malloc(sizeof(t_buffer));
+    paquete->buffer->size = 0;
+    paquete->buffer->stream = NULL;
+}
+
+t_paquete* packetCreate(void)
+{
+    t_paquete* paquete = malloc(sizeof(t_paquete));
+    paquete->codigo_operacion = PAQUETE;
+    crear_buffer(paquete);
+    return paquete;
+}
+
+void addToPacket(t_paquete* paquete, void* valor, int tamanio)
+{
+    paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
+
+    memcpy(paquete->buffer->stream + paquete->buffer->size, &tamanio, sizeof(int));
+    memcpy(paquete->buffer->stream + paquete->buffer->size + sizeof(int), valor, tamanio);
+
+    paquete->buffer->size += tamanio + sizeof(int);
+}
+
+void sendPacket(t_paquete* paquete, int socket_cliente)
+{
+    int bytes = paquete->buffer->size + 2*sizeof(int);
+    void* a_enviar = serializar_paquete(paquete, bytes);
+
+    send(socket_cliente, a_enviar, bytes, 0);
+
+    free(a_enviar);
+}
+
+void freePacket(t_paquete* paquete)
+{
+    free(paquete->buffer->stream);
+    free(paquete->buffer);
+    free(paquete);
+}
+
+void* getBuffer(int* size, int socket_cliente)
+{
+    void * buffer;
+
+    recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+    buffer = malloc(*size);
+    recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+    return buffer;
+}
+
+void* getMessage(int socket_cliente)
+{
+    int size;
+    char* buffer = getBuffer(&size, socket_cliente);
+    log_info(logger, "Me llego el mensaje %s", buffer);
+    //free(buffer);
+    pthread_exit((void*) buffer);
+}
+
+t_list* getPacket(int socket_cliente)
+{
+    int size;
+    int desplazamiento = 0;
+    void * buffer;
+    t_list* valores = list_create();
+    int tamanio;
+
+    buffer = getBuffer(&size, socket_cliente);
+    while(desplazamiento < size)
+    {
+        memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
+        desplazamiento+=sizeof(int);
+        char* valor = malloc(tamanio);
+        memcpy(valor, buffer+desplazamiento, tamanio);
+        desplazamiento+=tamanio;
+        list_add(valores, valor);
+    }
+    free(buffer);
+    return valores;
+}
+int getOp(int socket)
+{
+    int cod_op;
+    if(recv(socket, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+        return cod_op;
+    else
+    {
+        close(socket);
+        return -1;
+    }
+}
