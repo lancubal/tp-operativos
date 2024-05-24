4a4d4f0cb2435665c68ae6435d51cd45de618df2
Tom <tdemonasterio@frba.utn.edu.ar>
2024-05-24 20:35:10 +0000

    se envia un pcb correctamente en fifo

--- kernel/src/planificador.c
+++ kernel/src/planificador.c
@@ -48,40 +48,57 @@ void RR_ALGORITHM(t_queue * ready_queue, int quantum) {
     }
 }
 
-void FIFO_ALGORITHM(t_queue * ready_queue) {
-    t_PCB * pcb = queue_pop(ready_queue);
+void FIFO_ALGORITHM(t_queue * ready_queue, int quantum) {
+    t_PCB * pcb = malloc(sizeof(t_PCB));
+    pcb = queue_pop(ready_queue);
     t_queue *block_queue = queue_create();
     while (true) {
         if (pcb == NULL) {
-            pcb = queue_pop(block_queue);
+            if (queue_size(block_queue) == 0) {
+                break;
+            }
+            pcb = queue_pop(block_queue); // TODO no se deberia desbloquear si no llega una interrupcion.
             if (pcb == NULL) {
                 break;
             }
+            queue_push(ready_queue, pcb);
         }
         // Enviar PCB a CPU
+        send_opcode(sockets.dispatchSocket, PCB);
         send_tad(sockets.dispatchSocket, pcb, &pcb->size);
         pcb->State = "EXEC";
         log_info(logger, "Ejecutando proceso %d\n", pcb->PID);
-        recv_tad(sockets.dispatchSocket, (void **) &pcb, &pcb->size);
+        recv_tad(sockets.dispatchSocket, (void **) &pcb);
         // Tengo que decidir si lo mando a BLOCK o a EXIT
-        if (pcb->State == "BLOCK") {
+        if (strcmp(pcb->State, "BLOCK") == 0) {
             // Enviar PCB a CPU
             queue_push(block_queue, pcb);
             log_info(logger, "Proceso %d se bloqueo\n", pcb->PID);
-        } else if (pcb->State == "EXIT") {
+        } else if (strcmp(pcb->State, "EXIT") == 0) {
             log_info(logger, "Proceso %d termino\n", pcb->PID);
         }
-        free(pcb); // Liberar memoria del PCB (Proceso Control Block)
-        pcb = queue_pop(ready_queue);
+        if(queue_size(ready_queue) != 0) // La funcion queue_pop debería de implementar este checkeo
+            pcb = queue_pop(ready_queue);
+        else
+            pcb = NULL;
     }
+    free(pcb);
 }
 
-void SHORT_TERM_SCHEDULER(t_queue * ready_queue, ALGORITHM algo, int quantum) {
+void SHORT_TERM_SCHEDULER(t_queue * ready_queue, const char* algo, int quantum) {
+    ALGORITHM algorithm;
+    if (strcmp(algo, "VRR") == 0) {
+        algorithm = VRR_ALGORITHM;
+    } else if (strcmp(algo, "RR") == 0) {
+        algorithm = RR_ALGORITHM;
+    } else if (strcmp(algo, "FIFO") == 0) {
+        algorithm = FIFO_ALGORITHM;
+    }
     while (queue_size(ready_queue) > 0) {
-        algo(ready_queue);
+        algorithm(ready_queue, quantum);
     }
 }
 
-void LONG_TERM_SCHEDULER() {
+/*void LONG_TERM_SCHEDULER() {
     // Code
-}
+}*/
--- kernel/src/planificador.h
+++ kernel/src/planificador.h
@@ -12,6 +12,12 @@
 #include "sockets/protocol.h"
 #include "sockets/client.h"
 
-typedef void (*ALGORITHM)(t_queue *);
+typedef void (*ALGORITHM)(t_queue *, int);
+
+void SHORT_TERM_SCHEDULER(t_queue * ready_queue, const char* algo, int quantum);
+//void LONG_TERM_SCHEDULER(t_queue * ready_queue, t_queue * new_queue);
+void VRR_ALGORITHM(t_queue * ready_queue, int quantum);
+void RR_ALGORITHM(t_queue * ready_queue, int quantum);
+void FIFO_ALGORITHM(t_queue * ready_queue, int quantum);
 
 #endif //TP_2024_1C_GRUPO_PLANIFICADOR_H
